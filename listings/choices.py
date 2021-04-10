eb28e0cdb4122cc41723230c80202c97fa408ee6
Bobclint <bobclinton1998@gmail.com>
2021-04-10 11:01:46 +0300

    initial commit

---
+++ listings/choices.py
@@ -0,0 +1,46 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10
+}
+
+price_choices = {
+    '100000':'KSH 100000',
+    '200000':'KSH 200000',
+    '300000':'KSH 300000',
+    '400000':'KSH 400000',
+    '500000':'KSH 500000',
+    '600000':'KSH 600000',
+    '700000':'KSH 700000',
+    '800000':'KSH 800000',
+    '900000':'KSH 900000',
+    '1000000':'KSH 1M+',
+}
+state_choices = {
+    'BO': 'Bomet',
+    'BA': 'Baringo',
+    'BU': 'Busia',
+    'MB': 'Mombasa',
+    'BN': 'Bungoma',
+    'EM': 'Elgeyo-Marakwet',
+    'EM': 'Embu',
+    'GA': 'Garissa',
+    'DC': 'District of Columbia',
+    'HB': 'Homa Bay',
+    'KJ': 'Kajiado',
+    'KK': 'Kakamega',
+    'KM': 'Kisumu',
+    'KE': 'Kericho',
+    'KS': 'Kissi',
+    'KI': 'Kiambu',
+    'KF': 'Kilifi',
+    'KR': 'Kirinyaga',
+    
+}
